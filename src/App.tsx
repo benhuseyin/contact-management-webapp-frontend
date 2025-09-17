5aecb6789f1a784799d21938e98a7a9fc21119b0
Huseyin Kara <67840358+benhuseyin@users.noreply.github.com>
2025-09-17 20:30:37 +0300

    [FEATURE] Import button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { Fragment } from 'react/jsx-runtime'
 import './App.css'
+import { Button } from './components/ui/button'
 import { Input } from './components/ui/input'
 
 function App() {
 
   return (
-
-      <Input />
-
+      <Fragment>
+        <Input />
+        <Button>Login</Button>
+      </Fragment>
   )
 }
 
