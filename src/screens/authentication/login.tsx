dd05050b972135c084edeffaae2c54c69c669c91
Huseyin Kara <67840358+benhuseyin@users.noreply.github.com>
2025-09-19 01:10:57 +0300

    [FEATURE] Add pretier, and start to login screen

--- src/screens/authentication/login.tsx
+++ src/screens/authentication/login.tsx
@@ -1,6 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 const LoginScreen = () => {
-    return(
-        'Login'
+    return (
+        <div className="flex flex-col gap-y-20">
+            <div className="space-y-2.5">
+                <h1>Welcome Back!</h1>
+                <p>Enter Your Username & Password</p>
+            </div>
+
+            <form className="space-y-5">
+                <Input placeholder="E-mail" />
+                <Input placeholder="********" />
+                <Button>LOGIN</Button>
+            </form>
+        </div>
+
     );
 
 }
