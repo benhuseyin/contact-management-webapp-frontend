3f3a4666c800a93c7ce0437e896381998699e6b9
Huseyin Kara <67840358+benhuseyin@users.noreply.github.com>
2025-09-19 01:42:23 +0300

    [FEATURE] Complete login screen design

--- src/screens/authentication/login.tsx
+++ src/screens/authentication/login.tsx
@@ -1,21 +1,26 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import BgImage from "@/assets/images/LoginBackground.webp"
 
 const LoginScreen = () => {
     return (
         <div className="flex flex-col gap-y-20">
-            <div className="space-y-2.5">
+            <img
+                src={BgImage}
+                alt="Login background"
+                className="absolute left-1/2 top-1/2 -translate-1/2"
+            />
+            <div className="space-y-2.5 z-50">
                 <h1>Welcome Back!</h1>
                 <p>Enter Your Username & Password</p>
             </div>
 
-            <form className="space-y-5">
+            <form className="space-y-5 z-50">
                 <Input placeholder="E-mail" />
                 <Input placeholder="********" />
                 <Button>LOGIN</Button>
             </form>
         </div>
-
     );
 
 }
