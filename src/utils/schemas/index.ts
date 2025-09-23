c9b65bb7c146bf6cba5814a9c0d5442a31393115
Huseyin Kara <67840358+benhuseyin@users.noreply.github.com>
2025-09-23 13:33:55 +0300

    [REFACTOR] Create variable for regex, and use it

--- src/utils/schemas/index.ts
+++ src/utils/schemas/index.ts
@@ -1,7 +1,19 @@
 import z from "zod";
 
+export const emailRegex = /^(?!\.)(?!.*\.\.)([a-z0-9_'+\-\.]*)[a-z0-9_+-]@([a-z0-9][a-z0-9\-]*\.)+[a-z]{2,}$/i
+
 export const LoginSchema = z.object({
-    email: z.email({ message: 'Please entrey a valid email address.', pattern: /^(?!\.)(?!.*\.\.)([a-z0-9_'+\-\.]*)[a-z0-9_+-]@([a-z0-9][a-z0-9\-]*\.)+[a-z]{2,}$/i }).min(1, { message: "E-mail alanı boş bırakılamaz." }),
+    email: z.email({ message: 'Please entrey a valid email address.', pattern: emailRegex }).min(1, { message: "E-mail alanı boş bırakılamaz." }),
+    password: z.string()
+        .min(8, { message: "Password must be at least 8 characters long" })
+        .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
+        .regex(/[a-z]/, { message: "Password must contain at least one lowercase letter" })
+        .regex(/[0-9]/, { message: "Password must contain at least one number" })
+        .regex(/[^A-Za-z0-9]/, { message: "Password must contain at least one special character" }),
+});
+
+export const RegisterSchema = z.object({
+    email: z.email({ message: 'Please entrey a valid email address.', pattern: emailRegex }).min(1, { message: "E-mail alanı boş bırakılamaz." }),
     password: z.string()
         .min(8, { message: "Password must be at least 8 characters long" })
         .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
