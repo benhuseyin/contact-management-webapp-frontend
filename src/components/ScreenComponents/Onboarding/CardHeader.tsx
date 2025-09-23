492bf609b0b29710472b5c1e7b8b2903c8a3d7bb
Huseyin Kara <67840358+benhuseyin@users.noreply.github.com>
2025-09-23 13:49:44 +0300

    [REFACTOR] Add class prop to card header component

--- src/components/ScreenComponents/Onboarding/CardHeader.tsx
+++ src/components/ScreenComponents/Onboarding/CardHeader.tsx
@@ -1,11 +1,14 @@
+import { twMerge } from "tailwind-merge";
+
 interface Props {
+    classNames?: string;
     title: string;
     description: string;
 }
 
-const CardHeader = ({ title, description }: Props) => {
+const CardHeader = ({ classNames, title, description }: Props) => {
     return (
-        <div className="space-y-2.5 z-50">
+        <div className={twMerge("space-y-2.5 z-50", classNames)}>
             <h1>{title}</h1>
             <p>{description}</p>
         </div>
