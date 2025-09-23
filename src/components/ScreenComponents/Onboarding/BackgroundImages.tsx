4d171b67b5e5aab73e5ca0c011176470fb36b190
Huseyin Kara <67840358+benhuseyin@users.noreply.github.com>
2025-09-23 14:17:57 +0300

    [REFACTOR] Add media screen to change mobile sizes of h1, and p tags. Add sizes to background images

--- src/components/ScreenComponents/Onboarding/BackgroundImages.tsx
+++ src/components/ScreenComponents/Onboarding/BackgroundImages.tsx
@@ -22,12 +22,12 @@ const BackgroundImages = () => {
             <img
                 src={BgImage}
                 alt="Login background"
-                className="absolute left-1/2 top-1/2 -translate-1/2"
+                className="absolute left-1/2 top-1/2 -translate-1/2 w-[350px] h-[350px] sm:w-[500px] sm:h-[600px] md:w-[600px] md:h-[700px]"
             />
             <img
                 src={RedBgImage}
                 alt="Login background"
-                className={classNames("absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rotate-90", {
+                className={classNames("absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rotate-90 w-[350px] h-[350px] sm:w-[500px] sm:h-[600px] md:w-[600px] md:h-[700px]", {
                     "animate-spin": hasAnimation
                 })}
 
